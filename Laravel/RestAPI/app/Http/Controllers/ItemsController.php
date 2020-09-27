995630de8852a8448f80df785428308bda8eb69c
sandrosxila <sandrosxila@yahoo.com>
2020-09-28 00:59:00 +0400

    initial commit of RESTAPI

---
+++ Laravel/RestAPI/app/Http/Controllers/ItemsController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ItemsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $items = Item::get();
+
+        return response()->json($items);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'text' => 'required',
+            'body' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return ['response' => $validator->messages(), 'success' => 'false'];
+        }
+
+        $item = new Item();
+        $item->text = $request->input('text');
+        $item->body = $request->input('body');
+        $item->save();
+        return response()->json($item);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $item = Item::find($id);
+
+        return response()->json($item);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'text' => 'required',
+            'body' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return ['response' => $validator->messages(), 'success' => 'false'];
+        }
+
+        $item = Item::find($id);
+        $item->text = $request->input('text');
+        $item->body = $request->input('body');
+        $item->save();
+        return response()->json($item);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $item = Item::find($id);
+        $item->delete();
+        return ['response' => 'item deleted successfully', 'success' => true];
+    }
+}
